86d8b1262af298080f6c7cf28e10aa7b2957f830
starfish01 <patrick.labes@gmail.com>
2019-10-16 06:29:11 +1000

    Updating styling and adding buttons for ease of use

--- project/resources/views/projects/create.blade.php
+++ project/resources/views/projects/create.blade.php
@@ -6,14 +6,24 @@
 <form method="POST" action="/projects">
     {{ csrf_field() }}
 
-    <div>
-        <input type="text" placeholder="Project title" name="title">
+    <div class="field">
+        <label for="title" class="label">Title</label>
+        <div class="control">
+            <input type="text" placeholder="title" name="title" class="input">
+        </div>
     </div>
-    <div>
-        <textarea placeholder="description" name="description"></textarea>
+
+    <div class="field">
+        <label for="description" class="label">Description</label>
+        <div class="control">
+            <textarea placeholder="description" name="description" class="textarea"></textarea>
+        </div>
     </div>
-    <div>
-        <button type="submit">Create Project</button>
+
+    <div class="field">
+        <div class="control">
+            <button class="button is-link" type="submit">Create Project</button>
+        </div>
     </div>
 </form>
 @endsection('content')
--- project/resources/views/projects/index.blade.php
+++ project/resources/views/projects/index.blade.php
@@ -1,10 +1,29 @@
 @extends('layout')
 @section('content')
-    <h1 class="title">Projects</h1>
+<h1 class="title">Projects</h1>
 <hr>
-    <ul>
+
+<h4 class="title is-4">Projects Created</h4>
+<table class="table is-hoverable">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
         @foreach ($projects as $project)
-            <li>{{ $project->title }}</li>
+        <tr>
+            <td>{{ $project->title }}</td>
+            <td>{{ $project->description }}</td>
+            <td><a href="projects/{{ $project->id }}/edit">Edit</a></td>
+        </tr>
         @endforeach
-    </ul>
+    </tbody>
+</table>
+
+<a href="projects/create" class="btn is-link">Create Project</a>
+
+
 @endsection('content')
