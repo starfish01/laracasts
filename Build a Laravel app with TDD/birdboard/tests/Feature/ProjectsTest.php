4621839e6efae6867352adc1e6c6b6d32b8385d5
starfish01 <patrick.labes@gmail.com>
2019-10-27 16:31:22 +1000

    Starting a new module

---
+++ Build a Laravel app with TDD/birdboard/tests/Feature/ProjectsTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_a_user_can_create_a_project()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->sentence
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->post('/projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('projects')->assertSee($attributes['title']);
+    }
+
+    public function test_a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
+        $project = factory('App\Project')->create();
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+    public function test_a_project_requires_a_title()
+    {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
+}
